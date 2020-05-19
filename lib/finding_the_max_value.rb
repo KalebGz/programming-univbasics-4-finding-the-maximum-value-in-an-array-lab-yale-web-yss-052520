a45ca9ad176025ac4b60045c51e720b467801744
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 06:17:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(array)
   max = array[0]
   (array.length- 1).times{|index|
-    if(array[index + 1] > index)
+    if(array[index + 1] > array[index])
       max = array[index + 1]
     end
   }
