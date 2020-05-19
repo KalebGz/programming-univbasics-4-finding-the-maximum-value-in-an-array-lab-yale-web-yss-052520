e3b9674bdabdbaaeb89cf62f1d03271bb90b3642
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 06:15:38 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  max = array[0]
+  (array.length- 1).times{[index]
+    if(array[index + 1] > index)
+      max = array[index + 1]
+    end
+  }
+  max
 end
